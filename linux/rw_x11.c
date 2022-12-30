f1a2a403104c3608117f5101dbab24d096d3a156
Denis Pauk <pauk.denis@gmail.com>
2022-12-30 23:37:36 +0200

    Remove svga code

--- linux/rw_x11.c
+++ linux/rw_x11.c
@@ -43,6 +43,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdint.h>
+#include <sys/mman.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
