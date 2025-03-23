yquake2/yquake2remaster

gl1: add initial code to get count of frames

src/client/refresh/gl1/header/local.h
@@ -501,6 +501,7 @@ extern int RI_InitContext(void* win);
 extern void RI_BeginRegistration(const char *model);
 extern struct model_s * RI_RegisterModel(const char *name);
 extern struct image_s * RI_RegisterSkin(const char *name);
+extern const entity_framegroup_t * RI_GetFrameGroups(const char *name, int *num);
 extern void RI_SetSky(const char *name, float rotate, int autorotate, const vec3_t axis);
 extern void RI_EndRegistration(void);
 extern qboolean RI_IsVSyncActive(void);

src/client/vid/header/ref.h
@@ -188,6 +188,7 @@ typedef struct
 	void	(EXPORT *BeginRegistration) (const char *map);
 	struct model_s * (EXPORT *RegisterModel) (const char *name);
 	struct image_s * (EXPORT *RegisterSkin) (const char *name);
+	const entity_framegroup_t * (EXPORT *GetFrameGroups)(const char *name, int *num);
 
 	void	(EXPORT *SetSky) (const char *name, float rotate, int autorotate, const vec3_t axis);
 	void	(EXPORT *EndRegistration) (void);

src/game/g_obj.c
@@ -39,7 +39,28 @@
 void
 object_flame1_think(edict_t *self)
 {
-	self->s.frame = (self->s.frame + 1) % 64;
+	int num, i, ofs_frames = 0, num_frames = 1;
+	const entity_framegroup_t * frames;
+
+	frames = gi.SV_GetFrameGroups(self->s.modelindex, &num);
+	for (i = 0; i < num; i++)
+	{
+		if (!strcmp(frames[i].name, "frame"))
+		{
+			ofs_frames = frames[i].ofs;
+			num_frames = frames[i].num;
+			break;
+		}
+	}
+
+	i = self->s.frame - ofs_frames;
+	if (i < 0)
+	{
+		i = 0;
+	}
+	i++;
+
+	self->s.frame = ofs_frames + i % num_frames;
 	self->nextthink = level.time + FRAMETIME;
 }
 

src/game/g_spawn.c
@@ -183,12 +183,44 @@ dynamicspawn_touch(edict_t *self, edict_t *other, cplane_t *plane /* unused */,
 	gi.centerprintf(other, "Entity description: %s", self->message);
 }
 
+void
+dynamicspawn_think(edict_t *self)
+{
+	int num, i, ofs_frames = 0, num_frames = 1;
+	const entity_framegroup_t * frames;
+
+	frames = gi.SV_GetFrameGroups(self->s.modelindex, &num);
+	for (i = 0; i < num; i++)
+	{
+		if (!strcmp(frames[i].name, "frame"))
+		{
+			ofs_frames = frames[i].ofs;
+			num_frames = frames[i].num;
+			break;
+		}
+	}
+
+	i = self->s.frame - ofs_frames;
+	if (i < 0)
+	{
+		i = 0;
+	}
+	i++;
+
+	self->s.frame = ofs_frames + i % num_frames;
+	self->nextthink = level.time + FRAMETIME;
+}
+
 static void
 DynamicSpawn(edict_t *self, dynamicentity_t *data)
 {
 	/* All other properties could be updated in DynamicSpawnUpdate */
 	self->movetype = MOVETYPE_NONE;
 	self->solid = SOLID_BBOX;
+	self->think = dynamicspawn_think;
+	self->nextthink = level.time + FRAMETIME;
+
+	self->s.frame = 0;
 
 	/* set message only if it has description */
 	if (data->description[0])

src/game/header/game.h
@@ -214,7 +214,8 @@ typedef struct
 	void (*FS_FreeFile)(void *buf);
 	const char * (*FS_Gamedir)(void);
 	void (*FS_CreatePath)(const char *path);
-	const char * (*get_configstring)(int num);
+	const char * (*SV_GetConfigString)(int num);
+	const entity_framegroup_t * (*SV_GetFrameGroups)(int index, int *num);
 } game_import_t;
 
 /* functions exported by the game subsystem */

0lvin/heretic2

src/client/refresh/gl1/header/local.h
@@ -501,6 +501,7 @@ extern int RI_InitContext(void* win);
 extern void RI_BeginRegistration(const char *model);
 extern struct model_s * RI_RegisterModel(const char *name);
 extern struct image_s * RI_RegisterSkin(const char *name);
+extern const dmdxframegroup_t * RI_GetFrameGroups(const char *name, int *num);
 extern void RI_SetSky(const char *name, float rotate, int autorotate, const vec3_t axis);
 extern void RI_EndRegistration(void);
 extern qboolean RI_IsVSyncActive(void);

src/client/vid/header/ref.h
@@ -188,6 +188,7 @@ typedef struct
 	void	(EXPORT *BeginRegistration) (const char *map);
 	struct model_s * (EXPORT *RegisterModel) (const char *name);
 	struct image_s * (EXPORT *RegisterSkin) (const char *name);
+	const dmdxframegroup_t * (EXPORT *GetFrameGroups)(const char *name, int *num);
 
 	void	(EXPORT *SetSky) (const char *name, float rotate, int autorotate, const vec3_t axis);
 	void	(EXPORT *EndRegistration) (void);

src/game/g_obj.c
@@ -39,7 +39,28 @@
 void
 object_flame1_think(edict_t *self)
 {
-	self->s.frame = (self->s.frame + 1) % 64;
+	int num, i, ofs_frames = 0, num_frames = 1;
+	const dmdxframegroup_t * frames;
+
+	frames = gi.SV_GetFrameGroups(self->s.modelindex, &num);
+	for (i = 0; i < num; i++)
+	{
+		if (!strcmp(frames[i].name, "frame"))
+		{
+			ofs_frames = frames[i].ofs;
+			num_frames = frames[i].num;
+			break;
+		}
+	}
+
+	i = self->s.frame - ofs_frames;
+	if (i < 0)
+	{
+		i = 0;
+	}
+	i++;
+
+	self->s.frame = ofs_frames + i % num_frames;
 	self->nextthink = level.time + FRAMETIME;
 }
 

src/game/g_spawn.c
@@ -183,12 +183,44 @@ dynamicspawn_touch(edict_t *self, edict_t *other, cplane_t *plane /* unused */,
 	gi.centerprintf(other, "Entity description: %s", self->message);
 }
 
+void
+dynamicspawn_think(edict_t *self)
+{
+	int num, i, ofs_frames = 0, num_frames = 1;
+	const dmdxframegroup_t * frames;
+
+	frames = gi.SV_GetFrameGroups(self->s.modelindex, &num);
+	for (i = 0; i < num; i++)
+	{
+		if (!strcmp(frames[i].name, "frame"))
+		{
+			ofs_frames = frames[i].ofs;
+			num_frames = frames[i].num;
+			break;
+		}
+	}
+
+	i = self->s.frame - ofs_frames;
+	if (i < 0)
+	{
+		i = 0;
+	}
+	i++;
+
+	self->s.frame = ofs_frames + i % num_frames;
+	self->nextthink = level.time + FRAMETIME;
+}
+
 static void
 DynamicSpawn(edict_t *self, dynamicentity_t *data)
 {
 	/* All other properties could be updated in DynamicSpawnUpdate */
 	self->movetype = MOVETYPE_NONE;
 	self->solid = SOLID_BBOX;
+	self->think = dynamicspawn_think;
+	self->nextthink = level.time + FRAMETIME;
+
+	self->s.frame = 0;
 
 	/* set message only if it has description */
 	if (data->description[0])

src/game/header/game.h
@@ -214,7 +214,8 @@ typedef struct
 	void (*FS_FreeFile)(void *buf);
 	const char * (*FS_Gamedir)(void);
 	void (*FS_CreatePath)(const char *path);
-	const char * (*get_configstring)(int num);
+	const char * (*SV_GetConfigString)(int num);
+	const dmdxframegroup_t * (*SV_GetFrameGroups)(int index, int *num);
 } game_import_t;
 
 /* functions exported by the game subsystem */