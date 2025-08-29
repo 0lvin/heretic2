d7b2f8617bf4276c251f01e7930c9c97c8021e79
Denis Pauk <pauk.denis@gmail.com>
2025-08-30 00:25:21 +0300

    get rid pers from playerinfo

--- src/game/savegame/tables/clientfields.h
+++ src/game/savegame/tables/clientfields.h
@@ -29,7 +29,7 @@
 {"newweapon", CLOFS(newweapon), F_ITEM},
 {"owned_sphere", CLOFS(owned_sphere), F_EDICT},
 {"lastweapon", CLOFS(pers.lastweapon), F_ITEM},
-{"pers.weapon", CLOFS(playerinfo.pers.weapon), F_ITEM},
+{"weapon", CLOFS(pers.weapon), F_ITEM},
 {"resp.coop_respawn.lastweapon", CLOFS(resp.coop_respawn.lastweapon), F_ITEM, 0, 3},
 {"resp.coop_respawn.weapon", CLOFS(resp.coop_respawn.weapon), F_ITEM, 0, 3},
 {"defence", CLOFS(pers.defence), F_ITEM},
